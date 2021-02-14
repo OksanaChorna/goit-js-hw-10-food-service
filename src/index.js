2890b9fe89280ca6015995ab212971e90503a781
Oksana Chorna <ksuchorna@gmail.com>
2021-02-14 21:52:01 +0200

    Створено галерею меню

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+
+import menuItems from '../src/templates/menu-items.hbs';
+import menu from '../src/menu.json';
+
+const markup = menuItems(menu);
+const menuRef = document.querySelector('.js-menu');
+menuRef.insertAdjacentHTML('beforeend', markup);
